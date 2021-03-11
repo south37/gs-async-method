c232136abb17b1f0c435437205657ac002f3bd18
Nao Minami <south37777@gmail.com>
2021-03-12 01:07:53 +0900

    Add Java classes

--- initial/src/main/java/com/example/asyncmethod/AsyncMethodApplication.java
+++ initial/src/main/java/com/example/asyncmethod/AsyncMethodApplication.java
@@ -2,12 +2,30 @@ package com.example.asyncmethod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class AsyncMethodApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AsyncMethodApplication.class, args);
+		// close the application context to shut down the custom ExecutorService
+		SpringApplication.run(AsyncMethodApplication.class, args).close();
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(3);
+		executor.setMaxPoolSize(3);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GitHubLookup-");;
+		executor.initialize();
+		return executor;
 	}
 
 }
